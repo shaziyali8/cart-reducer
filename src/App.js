c7d799b8b3e405256de5386ec63b68ed559fdd72
shaziyali8 <sanakh8169@gmail.com>
2024-06-24 21:30:27 +0530

    solve issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Header from './components/Header';
 import Shop from './components/Shop';
 import { DUMMY_PRODUCTS } from './dummy-products';
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import { useContext, useRef } from 'react';
 
 import CartModal from './CartModal.js';
 import { CartCnxt } from '../store/shoppingCart-contextAPI.js';
-import {logo} from "../assets/logo.png" 
 
 export default function Header() {
   const { items } = useContext(CartCnxt);
